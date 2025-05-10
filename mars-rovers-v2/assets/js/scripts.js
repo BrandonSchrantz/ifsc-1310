7e2dd77032487ff4ed7c9f901b22d202cd4ceccf
BrandonSchrantz <baschrantz@ualr.edu>
2025-05-10 03:39:23 +0000

    Update

---
+++ mars-rovers-v2/assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('.fade').gallery({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+      /*slidesToShow: 2,
+      slidesToScroll: 2*/
+    });
+
+
+});
